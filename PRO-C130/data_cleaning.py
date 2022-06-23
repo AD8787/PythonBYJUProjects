5f9618f7a7e0195e2c404a69d9dc0c067a8119d2
AD8787 <75181003+AD8787@users.noreply.github.com>
2022-06-23 13:03:22 -0700

    Add files via upload

---
+++ PRO-C130/data_cleaning.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import csv
+
+df = pd.read_csv('Data_merged.csv')
+del df['Luminosity']
+del df['Star_name']
+del df['Distance']
+del df['Mass']
+del df['Radius']
+
+df.to_csv('main.csv')
